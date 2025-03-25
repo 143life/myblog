3eeddcb9d55b012ed6f7121745b4f2a240cfbdd4
Floccinau <argonavt28@mail.ru>
2025-03-25 19:27:38 +0300

    Checking the acquired knowledge about the general structure of the Django application

--- news/models.py
+++ news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+	'''Данные о новости'''
+	title = models.CharField('Заголовок', max_length=100)
+	description = models.TextField('Текст новости')
+	author = models.CharField('Автор', max_length=40)
+	datetime = models.DateTimeField('Дата и время')
+
+	def __str__(self):
+		return f'{self.title}, {self.author}'
+	
+	class Meta:
+		verbose_name = 'Новость'
+		verbose_name_plural = 'Новости'
--- news/views.py
+++ news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
+from .models import News
 
-# Create your views here.
+class NewsView(View):
+	def get(self, request, *args, **kwargs):
+		news = News.objects.all()
+		return render(request, "news/news.html", {'news_list': news})
